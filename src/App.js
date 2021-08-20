5546f66313436e4dbbe4c274d0b73e9cf2683e5d
Kris <kristof.solak@gmail.com>
2021-08-20 07:39:26 +0100

    Router added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
+import {Navbar} from "./Components/Navbar";
+import {Main} from "./Components/Main";
+import {Order} from "./Components/Order";
+import {About} from "./Components/About";
+import {Contact} from "./Components/Contact";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+          <Navbar/>
+          <Route exact path="/" component={Main} />
+          <Route path="/order"  component={Order} />
+          <Route path="/contact"  component={Contact} />
+          <Route path="/about"  component={About} />
+      </div>
+    </Router>
   );
 }
 
