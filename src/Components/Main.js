b752ceddc9bdcac2e95a352c90627e683176040e
Kris <kristof.solak@gmail.com>
2021-08-21 11:38:50 +0100

    Basic Main component created

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -2,8 +2,14 @@ import React from 'react'
 
 export const Main = () => {
     return (
-        <div>
-            Main
+        <div className="main">
+           <div className="wrapper">
+               <div className="mainImage"></div>
+               <div className="main grid">        
+                        <h1>Sadipscing lorem ut sea.</h1>
+                        <input type="submit" value="Create" class="btn btn-primary"></input>
+                </div>
+           </div>
         </div>
     )
 }
