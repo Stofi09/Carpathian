9ef4f22530299f180fc5be3bf58739787a329690
Kris <kristof.solak@gmail.com>
2021-08-23 15:59:31 +0100

    Add and remove added to change the food.amount.

--- src/Components/FoodComponent.js
+++ src/Components/FoodComponent.js
@@ -3,22 +3,24 @@ import IconButton from '@material-ui/core/IconButton';
 import AddButton from '@material-ui/icons/Add';
 import RemoveButton from '@material-ui/icons/Remove';
 
-export const FoodComponent = ({Name, Description}) => {
+export const FoodComponent = ({Food, addFood, removeFood}) => {
+    
     const removeOnClick = () => {
-        console.log("asd");
+        removeFood(Food.id);
     }
 
     const AddOnClick = () => {
-        console.log("asd");
+        addFood(Food.id);
     }
 
     return (
         <div className="form m-2">
             <div className="grid">
-                <h3>{Name} {Description} : </h3>
+                <h3>{Food.id} {Food.main} : </h3>
                 <IconButton onClick={removeOnClick} aria-label="delete">
                     <RemoveButton />
                 </IconButton>
+                <h3>{Food.amount}</h3>
                 <IconButton onClick={AddOnClick} aria-label="delete">
                     <AddButton />
                 </IconButton>
--- src/Components/Order.js
+++ src/Components/Order.js
@@ -3,27 +3,51 @@ import { FoodComponent } from './FoodComponent';
 
 export const Order = () => {
 
-    const [food, setFood] = useState({
-        menu1: 
-        [
-            {
-                id:1,
-                main:"hamburger"
-            },
-            {
-                id:2,
-                main:"cheeseburger"    
-            }
-        ],
-    })
+    const [foods, setFoods] = useState([
+        {
+            id:0,
+            main:"hamburger",
+            amount:0
+        },
+        {
+            id:1,
+            main:"cheeseburger",
+            amount:0   
+        },
+        {
+            id:2,
+            main:"vegburger",
+            amount:0
+        }
+    ])
+    
+    
+    const addFood = (id) =>{
+        const updateFoods = [...foods];
+        updateFoods[id].amount++;
+        setFoods(updateFoods)
+    }
+
+    const removeFood = (id) => {
+        const updateFoods = [...foods];
+        if (updateFoods[id].amount > 0){
+            updateFoods[id].amount--;
+        }
+        setFoods(updateFoods)
+    }
     return (
         <div className="order">
            <div className="flex p-2">
                <div className="text">
                     <h1>Order online!</h1>
                     <h4>Scouuo urumemtuul kyul hullothya iunhum scouuo urumemtuul thekunched buthuruth fyaal werethul. Kethwe kyth fugwa kynaal ualmun buthuruth. Hyul scegenul kunuel.</h4>
-                    <FoodComponent Name = {food.menu1[0].id}
-                    Description={food.menu1[0].main}/>
+                    { foods.map((food)=>
+                        <FoodComponent
+                        Food = {food}
+                        addFood = {addFood}
+                        removeFood = {removeFood}
+                        />
+                    )}
                 </div>
            </div>
         </div>
