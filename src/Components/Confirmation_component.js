73a2d955abb759e203a5e87b8a4c430fdae4ddba
Kris <kristof.solak@gmail.com>
2021-09-02 18:52:29 +0100

    Warning added when no food added yet.

--- src/Components/Confirmation_component.js
+++ src/Components/Confirmation_component.js
@@ -1,14 +1,25 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 export const Confirmation_component = ({foods}) => {
+    const [isEmpty, setIsEmpty] = useState(false);
+
+    useEffect(() => {
+        foods.map((food)=>{
+            if (food. amount > 0){
+                setIsEmpty(true);
+                console.log("true");
+            }
+        })
+    }, [])
+
     return (
         <div className="confirmation">
             <div className="form">
-            {
-            foods.filter(food => food.amount > 0).map(filteredFood =>(
-               
-               <h3>{filteredFood.amount}X {filteredFood.main}</h3>
-            ))
+            { isEmpty? 
+                foods.filter(food => food.amount > 0).map(filteredFood =>(
+                <h3>{filteredFood.amount}X {filteredFood.main}</h3>
+            ))       : 
+                <div> You haven't chosen a food yet.</div>
             }
             </div>
         </div>
